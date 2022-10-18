e1b7ba87963d0cc98f01371d3c8c606705e8026f
esther921 <estherniyonkuru51@gmail.com>
2022-10-18 17:50:19 +0200

    add vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,6 @@ module.exports = {
       boxShadow: {
         'allsides': '1px 1px 4px 1px rgba(0, 0, 0, 0.3)',
       },
-      screens: {
-        'mediu': { 'max': '800px' }
-      }
     },
   },
   plugins: [],

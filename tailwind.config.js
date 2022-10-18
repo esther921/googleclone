677033e04a838eff0f4c7a91df5df8e4e52fe4e8
esther921 <estherniyonkuru51@gmail.com>
2022-10-18 19:30:33 +0200

    Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    spacing: {
+      '13': '52px',
+    },
     extend: {
       boxShadow: {
         'allsides': '1px 1px 4px 1px rgba(0, 0, 0, 0.3)',
       },
+      
     },
   },
   plugins: [],

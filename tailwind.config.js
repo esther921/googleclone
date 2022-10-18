e94db5588dcd1dcb7a9808469a051030d47dd79a
esther921 <estherniyonkuru51@gmail.com>
2022-10-18 17:33:49 +0200

    Added Responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-
     extend: {
       boxShadow: {
         'allsides': '1px 1px 4px 1px rgba(0, 0, 0, 0.3)',
+      },
+      screens: {
+        'mediu': { 'max': '800px' }
       }
     },
   },

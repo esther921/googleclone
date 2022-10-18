be790bfef8d7a8734c3cc73af828ad7296b665be
esther921 <estherniyonkuru51@gmail.com>
2022-10-18 14:34:50 +0200

    Google desktop version

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+
+    extend: {
+      boxShadow: {
+        'allsides': '1px 1px 4px 1px rgba(0, 0, 0, 0.3)',
+      }
+    },
+  },
+  plugins: [],
+}
